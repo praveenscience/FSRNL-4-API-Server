3fa19435239d8987c9a41a0aa52e92435c780115
Praveen Kumar <praveenscience@gmail.com>
2021-05-23 16:38:51 +0100

    Created a basic Express JS Server.

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-console.log("Hello Server!");
+// Include Express JS.
+const express = require("express");
+// Let's create a new Express Instance.
+const app = express();
+// Listen it in a particular port.
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Server Started in Port ${port}.`);
+});
