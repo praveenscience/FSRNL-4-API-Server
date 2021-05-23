4e810080fc407a9adc93da937f97820cb0cbf0de
Praveen Kumar <praveenscience@gmail.com>
2021-05-23 16:44:00 +0100

    Implemented GET request handler for root.

--- server.js
+++ server.js
@@ -2,9 +2,14 @@
 const express = require("express");
 // Let's create a new Express Instance.
 const app = express();
-// Listen it in a particular port.
 const port = 3000;
 
+// Create a GET Request Handler for /.
+app.get("/", (req, res) => {
+  res.json("Hello Everyone!");
+});
+
+// Listen it in a particular port.
 app.listen(port, () => {
   console.log(`Server Started in Port ${port}.`);
 });
