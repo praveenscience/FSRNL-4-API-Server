9f24e806f5c45969f4d47b04a4ac6b04f14ab5c8
Praveen Kumar <praveenscience@gmail.com>
2021-05-23 18:23:53 +0100

    Added the old Users code.

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,8 +2,22 @@ const fs = require("fs");
 const express = require("express");
 const app = express.Router();
 
+const Users = [];
 app.get("/", (req, res) => {
   res.json("You're in Users!");
+});
+app.post("/", (req, res) => {
+  const { Name } = req.body;
+  if (!Name) {
+    res.status(400).json(`Where's Name?`);
+  } else {
+    if (Users.indexOf(Name) > -1) {
+      res.status(409).json(`${Name} already exists.`);
+    } else {
+      Users.push(Name);
+      res.status(201).json(`Added ${Name}.`);
+    }
+  }
 });
 app.get("/new", (req, res) => {
   res.json("Please use the POST request!");
